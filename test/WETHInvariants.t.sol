f4fe85b1a74c6be971497be223f2bb7020568058
clement-ux <clemmoller@gmail.com>
2023-02-16 16:33:35 +0100

    final commit

--- test/WETHInvariants.t.sol
+++ test/WETHInvariants.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.17;
 
 import {Test} from "forge-std/Test.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
-import {Handler} from "test/Handler.t.sol";
+import {Handler} from "test/handler/Handler.t.sol";
 
 contract WETHInvariants is Test {
     WETH public weth;
@@ -11,12 +11,60 @@ contract WETHInvariants is Test {
 
     function setUp() public {
         weth = new WETH();
-        handler = new Handler(address(weth));
+        handler = new Handler(weth);
+
+        bytes4[] memory selectors = new bytes4[](6);
+        selectors[0] = Handler.deposit.selector;
+        selectors[1] = Handler.withdraw.selector;
+        selectors[2] = Handler.sendFallback.selector;
+        selectors[3] = Handler.approve.selector;
+        selectors[4] = Handler.transfer.selector;
+        selectors[5] = Handler.transferFrom.selector;
+
+        targetSelector(FuzzSelector({
+            addr: address(handler),
+            selectors: selectors
+        }));
+
 
         targetContract(address(handler));
     }
 
-    function invariant_badInvariantThisShouldFail() public {
-        assertEq(0, weth.totalSupply());
+    function invariant_conservationOfETH() public {
+        assertEq(handler.ETH_SUPPLY(), address(handler).balance + weth.totalSupply());
+    }
+
+    function invariant_solvencyDeposit() public {
+        assertEq(address(weth).balance, handler.ghost_depositSum() - handler.ghost_withdrawSum());
+    }
+
+    function invariant_solvencyBalances() public {
+        uint256 sumOfBalances = handler.reduceActors(
+          0,
+          this.accumulateBalance
+        );
+        assertEq(
+            address(weth).balance,
+            sumOfBalances
+        );
+    }
+
+    function invariant_depositorBalances() public {
+        handler.forEachActor(this.assertAccountBalanceLteTotalSupply);
+    }
+
+    function invariant_callSummary() public view {
+        handler.callSummary();
+    }
+
+    function assertAccountBalanceLteTotalSupply(address account) external {
+        assertLe(weth.balanceOf(account), weth.totalSupply());
+    }
+
+    function accumulateBalance(
+      uint256 balance,
+      address caller
+    ) external view returns (uint256) {
+        return balance + weth.balanceOf(caller);
     }
 }
---
+++ test/handler/Handler.t.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import {WETH} from "solmate/tokens/WETH.sol";
+import {LibAddressSet, AddressSet} from "test/helper/LibAddressSet.sol";
+
+contract Handler is Test {
+    using LibAddressSet for AddressSet;
+
+    AddressSet internal _actors;
+    
+    WETH public weth;
+    address internal currentActor;
+
+    uint256 public constant ETH_SUPPLY = 120_500_000 ether;
+    uint256 public ghost_depositSum;
+    uint256 public ghost_withdrawSum;
+    uint256 public ghost_zeroWithdrawals;
+
+
+    mapping(bytes32 => uint256) public calls;
+
+    modifier useActor(uint256 actorIndexSeed) {
+        currentActor = _actors.rand(actorIndexSeed);
+        _;
+    }
+
+    modifier countCall(bytes32 key) {
+        calls[key]++;
+        _;
+    }
+
+    modifier createActor() {
+        currentActor = msg.sender;
+        _actors.add(msg.sender);
+        _;
+    }
+
+    constructor(WETH _weth) {
+        weth = _weth;
+        deal(address(this), ETH_SUPPLY);
+    }
+
+    function deposit(uint256 amount) public createActor countCall("deposit") {
+        amount = bound(amount, 0, address(this).balance);
+        _pay(currentActor, amount);
+
+        vm.prank(currentActor);
+        weth.deposit{value: amount}();
+
+        ghost_depositSum += amount;
+    }
+
+    function withdraw(uint256 actorSeed, uint256 amount) public useActor(actorSeed) countCall("withdraw") {
+        amount = bound(amount, 0, weth.balanceOf(currentActor));
+        if (amount == 0) ghost_zeroWithdrawals++;
+
+        vm.startPrank(currentActor);
+        weth.withdraw(amount);
+        _pay(address(this), amount);
+        vm.stopPrank();
+
+        ghost_withdrawSum += amount;
+    }
+
+    function sendFallback(uint256 amount) public createActor countCall("sendFallback") {
+        amount = bound(amount, 0, address(this).balance);
+        _pay(currentActor, amount);
+
+        vm.prank(currentActor);
+        (bool success,) = address(weth).call{value: amount}("");
+
+        require(success, "sendFallback failed");
+        ghost_depositSum += amount;
+    }
+
+    function approve(
+        uint256 actorSeed,
+        uint256 spenderSeed,
+        uint256 amount
+    ) public useActor(actorSeed) countCall("approve") {
+        address spender = _actors.rand(spenderSeed);
+
+        vm.prank(currentActor);
+        weth.approve(spender, amount);
+    }
+
+    function transfer(
+        uint256 actorSeed,
+        uint256 toSeed,
+        uint256 amount
+    ) public useActor(actorSeed) countCall("transfer") {
+        address to = _actors.rand(toSeed);
+
+        amount = bound(amount, 0, weth.balanceOf(currentActor));
+
+        vm.prank(currentActor);
+        weth.transfer(to, amount);
+    }
+
+    function transferFrom(
+        uint256 actorSeed,
+        uint256 fromSeed,
+        uint256 toSeed,
+        bool _approve,
+        uint256 amount
+    ) public useActor(actorSeed) countCall("transferFrom")
+    {
+        address from = _actors.rand(fromSeed);
+        address to = _actors.rand(toSeed);
+
+        amount = bound(amount, 0, weth.balanceOf(from));
+
+        if (_approve) {
+            vm.prank(from);
+            weth.approve(currentActor, amount);
+        } else {
+            amount = bound(amount, 0, weth.allowance(currentActor, from));
+        }
+
+        vm.prank(currentActor);
+        weth.transferFrom(from, to, amount);
+    }
+
+    function _pay(address to, uint256 amount) internal {
+        (bool s,) = to.call{value: amount}("");
+        require(s, "pay() failed");
+    }
+
+    function actors() external returns (address[] memory) {
+        return _actors.addrs;
+    }
+
+    function forEachActor(function(address) external func) public {
+        return _actors.forEach(func);
+    }
+
+    function reduceActors(
+        uint256 acc,
+        function(uint256,address) external returns (uint256) func
+    )
+        public
+        returns (uint256)
+    {
+        return _actors.reduce(acc, func);
+    }
+
+    function callSummary() external view {
+        console.log("Call summary:");
+        console.log("-------------------");
+        console.log("deposit:", calls["deposit"]);
+        console.log("withdraw:", calls["withdraw"]);
+        console.log("sendFallback:", calls["sendFallback"]);
+        console.log("-------------------");
+
+        console.log("Zero withdrawals:", ghost_zeroWithdrawals);
+    }
+
+
+    receive() external payable {}
+}
